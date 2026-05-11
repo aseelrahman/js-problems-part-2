bc94d7f6ca73fbfda8e73058e1929ba0d143599b
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-11 07:52:11 -0400

    shopping cart calculator

---
+++ total.js
@@ -0,0 +1,19 @@
+const numbers = [300, 100, 700, 1200];
+
+const products = [
+    {name: 'shampoo', price: 300},
+    {name: 'chiruni', price: 100},
+    {name: 'shirt', price: 700},
+    {name: 'pant', price: 1200},
+]
+
+function getShoppingTotal(products){
+    let sum = 0;
+    for(const product of products){
+        sum = sum + product.price;
+    }
+    return sum;
+}
+
+const total = getShoppingTotal(products);
+console.log('Total ajke khosabe: ', total);
