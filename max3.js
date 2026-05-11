b9c704e58b8a72ab88847f7042594a3c1c62d479
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-11 06:42:13 -0400

    find max number in js

---
+++ max3.js
@@ -0,0 +1,20 @@
+const jim = 56;
+const tim = 89;
+const kim = 68;
+
+if(jim > tim && jim > kim){
+    console.log('Jim is the ultimate boss');
+}
+else if(tim > jim && tim > kim){
+    console.log('Tim is the boss');
+}
+else{
+    console.log('Kim is the kardashians boss');
+}
+
+function maxOfThree (num1, num2, num3){
+    // do it yourself
+}
+
+const max = Math.max(12, 1, 56, 5, 65, 7)
+console.log('Max using Math.max', max);
